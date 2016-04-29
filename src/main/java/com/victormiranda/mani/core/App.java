70666c0a9fefa043de0440685efd659a77a50c64
Victor Miranda <vmiranda@vsware.ie>
2016-04-29 13:45:02 +0100

    corsss

--- src/main/java/com/victormiranda/mani/core/App.java
+++ src/main/java/com/victormiranda/mani/core/App.java
@@ -3,12 +3,26 @@ package com.victormiranda.mani.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class App {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(App.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/transactions").allowedOrigins("*");
+			}
+		};
 	}
 
 

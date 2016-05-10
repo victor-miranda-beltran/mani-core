323e056cb46f273b0c2b7c9768d3d1163fdaa6bb
Victor MIranda <victormiranda@gmail.com>
2016-05-10 17:47:20 +0100

    pending transactions could have empty uids

--- src/main/java/com/victormiranda/mani/core/dao/bankaccount/TransactionDao.java
+++ src/main/java/com/victormiranda/mani/core/dao/bankaccount/TransactionDao.java
@@ -14,6 +14,6 @@ public interface TransactionDao extends CrudRepository<BankTransaction, Integer>
 	@Query("select t from BankTransaction t where t.bankAccount.bankLogin.user.id = ?1")
 	List<BankTransaction> getTransactions(final Integer userId);
 
-	@Query("select t from BankTransaction t where t.uid = ?2 and t.bankAccount.bankLogin.user.id = ?1")
+	@Query("select t from BankTransaction t where t.uid = ?2 and t.uid != '' and t.bankAccount.bankLogin.user.id = ?1")
 	Optional<BankTransaction> findByUserAndUID(final Integer userId, final String uid);
 }

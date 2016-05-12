549aebe7dcc4ba69c154b8604d1abc92265574b6
Victor MIranda <victormiranda@gmail.com>
2016-05-12 14:04:30 +0100

    LocalDateAttConverter test

---
+++ src/test/java/com/victormiranda/mani/core/test/ut/inputtransformer/impl/ptsb/LocalDateAttConverterTest.java
@@ -0,0 +1,23 @@
+package com.victormiranda.mani.core.test.ut.inputtransformer.impl.ptsb;
+
+import com.victormiranda.mani.core.LocalDateAttributeConverter;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+public class LocalDateAttConverterTest {
+
+    @Test
+    public void testConversionInTwoDirections() {
+        final LocalDate localDateNow = LocalDate.now();
+
+        final LocalDateAttributeConverter converter = new LocalDateAttributeConverter();
+        final Date dateFromLocalDate = converter.convertToDatabaseColumn(localDateNow);
+
+        final LocalDate localDateAfterConversions = converter.convertToEntityAttribute(dateFromLocalDate);
+
+        Assert.assertEquals(localDateNow, localDateAfterConversions);
+    }
+}
